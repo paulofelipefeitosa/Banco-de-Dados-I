526d2081eaea80b00c3f655a80d0b8673a6ceb8a
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2016-08-24 15:30:24 -0300

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,6 @@
+INSERT INTO Funcionario 
+VALUES (3,'Janet Leverling Follet','F','S',
+'4572457/SC','123658123-98','Srta','30/08/1963','70','01/04/1992','R. Flávio Melo Uchoa, 72','Apto. 502','Bessa',7,'58037-100','246.9008','982.1333',4,'MKT','5100','lever@tastrede.com.br')
+
+
+SET DATEFORMAT dmy
